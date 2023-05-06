dc702d8ee25199ef2492c1b244714845c2015070
davi-canuto <davicanutogregorio@gmail.com>
2023-05-06 18:47:32 -0300

    improvement: test if return svg content type'

--- src/components/SvgComponent.js
+++ src/components/SvgComponent.js
@@ -1,8 +1,6 @@
-import React from 'react';
-
 const SvgComponent = () => (
-  <svg width="100" height="100">
-    <circle cx="50" cy="50" r="40" stroke="green" strokeWidth="4" fill="yellow" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="80" height="80">
+    <circle cx="40" cy="40" r="40" fill="#F44336" />
   </svg>
 );
 
